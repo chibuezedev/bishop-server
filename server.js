24abe32ae786153e2b03f9e9f0edf6211a269b1d
Paul Chibueze <94766465+chibuezedev@users.noreply.github.com>
2024-03-07 14:39:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the root page!");
-}
+})
         
 app.post("/submit-form", (req, res) => {
   const { email, password } = req.body;
